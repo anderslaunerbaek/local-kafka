135a94752630175820c6e8159c64cba3ae30706a
Anders Launer Bæk-Petersen <anders@launer.dk>
2023-04-20 11:37:23 +0200

    jdbc sink and source.

--- hack/connect/run.sh
+++ hack/connect/run.sh
@@ -13,4 +13,4 @@ HOST_URL="http://dsnvm3.baekpetersen.dk:8083"
 
 curl -X "POST" "${HOST_URL}/connectors" \
      -H "Content-Type: application/json" \
-     -d @postgress_config.json
+     -d @source_postgress_config.json
